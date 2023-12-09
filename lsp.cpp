45862a0fb93b118b69229f35753c1f836ff62113
shiv <yadav.shivkumar5@gmail.com>
2023-12-09 20:54:15 +0530

    first commit

---
+++ lsp.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <exception>
+	
+/*
+class Bird {
+    public:
+        virtual void fly() = 0;
+};
+
+class Crow : public Bird {
+    public:
+        void fly() override {
+            std::cout << "Crow is flying" << std::endl;
+        }
+};
+
+class Penguin : public Bird {
+    public:
+        void fly() override {
+            throw std::runtime_error("Penguins can't fly");
+        }
+};
+
+void makeBirdFly(Bird& bird) {
+    bird.fly();
+}
+*/
+
+class Bird {
+    public:
+        virtual bool canFly() = 0;
+};
+
+class FlyingBird: public Bird {
+    public:
+        bool canFly() override {
+            return true;
+        }
+        virtual void fly() = 0;
+};
+
+class Crow : public FlyingBird {
+    public:
+        void fly() override {
+            std::cout << "Crow is flying" << std::endl;
+        }
+};
+
+class Penguin : public Bird {
+    public:
+        bool canFly() override {
+            return false;
+        }
+};
+
+void makeBirdFly(FlyingBird& bird) {
+    bird.fly();
+}
+
+int main()
+{
+
+Crow cr;
+makeBirdFly(cr);
+
+Penguin pn;
+//makeBirdFly(pn);  //compile time error
+
+    return 0;
+}
