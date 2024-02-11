c4d62bf3e4833ff65068d86a525b53778790d660
Shiv <yadav.shivkumar5@gmail.com>
2024-02-11 21:38:00 +0530

    Update dip.cpp

--- dip.cpp
+++ dip.cpp
@@ -1,4 +1,5 @@
 /*
+High-level modules should not depend on low-level modules. Both should depend on abstractions.
 Abstractions should not depend on details. Details should depend on abstractions.
 */
 
